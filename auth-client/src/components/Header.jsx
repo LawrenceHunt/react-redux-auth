a33221094619303332bd8572e3d57c165abae5ae
Lawrence Hunt <lawrence.hunt@hotmail.com>
2018-10-11 16:22:34 +0100

    added basic react components for auth

---
+++ auth-client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import {Link}             from 'react-router-dom'
+
+
+export default class extends Component {
+  render() {
+    return (
+      <div>
+        <Link to={'/'}>Redux Auth</Link>
+        <Link to={'/signup'}>Sign up</Link>
+        <Link to={'/signin'}>Sign in</Link>
+        <Link to={'/signout'}>Sign out</Link>
+        <Link to={'/feature'}>Feature</Link>
+      </div>
+    )
+  }
+}
---
+++ auth-client/src/components/auth/Signup.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+export default class extends Component {
+  render() {
+    return (
+      <form>
+        <fieldset>
+          <label>Email:</label>
+
+
+        </fieldset>
+        <fieldset>
+          <label>Password:</label>
+
+
+        </fieldset>
+      </form>
+    )
+  }
+}
